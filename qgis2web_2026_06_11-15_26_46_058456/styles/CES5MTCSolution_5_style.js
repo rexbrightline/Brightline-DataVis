c2ed223b9f2b3c1362f309ff82e0f7f59dc63b38
rexbrightline <rex@brightlinedefense.org>
2026-06-11 15:27:44 -0700

    CES 3 4 5 Map 2
    Added MTC solution

---
+++ qgis2web_2026_06_11-15_26_46_058456/styles/CES5MTCSolution_5_style.js
@@ -0,0 +1,62 @@
+var size = 0;
+var placement = 'point';
+
+var style_CES5MTCSolution_5 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("var value = '';");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+        function rules_CES5MTCSolution_5(feature, value) {
+            var context = {
+                feature: feature,
+                variables: {}
+            };
+            // Start of if blocks and style check logic
+            if (exp_CES5MTCSolution_5rule0_eval_expression(context)) {
+                      return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(166,206,227,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    }
+                    else if (exp_CES5MTCSolution_5rule1_eval_expression(context)) {
+                      return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(31,120,180,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    }
+                    else if (exp_CES5MTCSolution_5rule2_eval_expression(context)) {
+                      return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(168,168,168,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    }
+            else {
+                return [];
+            }
+        }
+        var style = rules_CES5MTCSolution_5(feature, value);
+        ;
+
+    return style;
+};
